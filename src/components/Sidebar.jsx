149db995609cc21388f3e29bd77e8d54d3ea00b2
Andres Briend <briendandres@gmail.com>
2024-02-27 20:52:38 -0300

    2

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,20 +1,53 @@
 import React from 'react'
-import { RiHome2Line } from "react-icons/ri";
+import { RiHome2Line,RiPercentLine,RiPieChartLine,RiMailLine,RiNotification3Line, RiSettings4Line} from "react-icons/ri";
 
 const Sidebar = () => {
-  return (
-    <div className="bg-[#1F1D2B] fixed left-0 top-0 w-28 h-full">
-        <h1 className='text-2xl text-gray-300 uppercase font-bold text-center my-5'>Logo</h1>
-        <ul className='pl-4'>
-            <li className='bg-[#262837] p-4 rounded-tl-x rounded-bl-xl'>
-                <a href='#' className='bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-white'>
-                    <RiHome2Line className='text-xl' />
-                </a>
-            </li>
-        </ul>
-    </div>
-    
-  )
+    return (
+        <div className="bg-[#1F1D2B] fixed left-0 top-0 w-28 h-full">
+            <ul className='pl-4'>
+                <li>
+                    <h1 className='text-2xl text-gray-300 uppercase font-bold text-center my-5'>Logo</h1>
+                </li>
+                <li className='bg-[#262837] p-4 rounded-tl-x rounded-bl-xl'>
+                    <a href='#' className='bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-white'>
+                        <RiHome2Line className='text-xl' />
+                    </a>
+                </li>
+
+                <li className='hover:bg-[#262837] p-4 rounded-tl-x rounded-bl-xl group transition-colors'>
+                    <a href='#' className='p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:bg-[#ec7c6a] group-hover:text-white transition-colors'>
+                        <RiPercentLine className='text-xl' />
+                    </a>
+                </li>
+
+                <li className='hover:bg-[#262837] p-4 rounded-tl-x rounded-bl-xl group transition-colors'>
+                    <a href='#' className='p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:bg-[#ec7c6a] group-hover:text-white transition-colors'>
+                        <RiPieChartLine className='text-xl' />
+                    </a>
+                </li>
+
+                <li className='hover:bg-[#262837] p-4 rounded-tl-x rounded-bl-xl group transition-colors'>
+                    <a href='#' className='p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:bg-[#ec7c6a] group-hover:text-white transition-colors'>
+                        <RiMailLine className='text-xl' />
+                    </a>
+                </li>
+
+                <li className='hover:bg-[#262837] p-4 rounded-tl-x rounded-bl-xl group transition-colors'>
+                    <a href='#' className='p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:bg-[#ec7c6a] group-hover:text-white transition-colors'>
+                        <RiNotification3Line className='text-xl' />
+                    </a>
+                </li>
+
+                <li className='hover:bg-[#262837] p-4 rounded-tl-x rounded-bl-xl group transition-colors'>
+                    <a href='#' className='p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:bg-[#ec7c6a] group-hover:text-white transition-colors'>
+                        <RiSettings4Line className='text-xl' />
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+
+    )
 }
 
 export default Sidebar
