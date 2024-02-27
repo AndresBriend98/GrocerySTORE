40738eab4e7720c680c101dd729b27995e7ec279
Andres Briend <briendandres@gmail.com>
2024-02-27 20:21:51 -0300

    Starting

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "./components/Sidebar"
+
+function App() {
+
+  return (
+      <div>
+        <Sidebar />
+      </div>
+  )
+}
+
+export default App
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className="bg-[#1F1D2B] fixed left-0 top-0 w-28">Sidebar
+    </div>
+    
+  )
+}
+
+export default Sidebar
