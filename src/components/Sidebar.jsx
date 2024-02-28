dd5ac5d6575e8241bc0aace47709f1d6b9a565e5
Andres Briend <briendandres@gmail.com>
2024-02-28 18:08:06 -0300

    5

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import { RiHome2Line, RiPercentLine, RiPieChartLine, RiMailLine, RiNotification3Line, RiSettings4Line, RiLoginCircleLine} from "react-icons/ri";
 
-const Sidebar = () => {
+const Sidebar = (props) => {
+
+    const {showMenu} = props;
+
     return (
-        <div className="bg-[#1F1D2B] fixed lg:left-0 top-0 w-28 h-full flex flex-col justify-between py-6 rounded-tr-xl rounded-br-xl z-50 -left-full">
+        <div className={`bg-[#1F1D2B] fixed lg:left-0 top-0 w-28 h-full flex flex-col justify-between py-6 rounded-tr-xl rounded-br-xl z-50 transition-all 
+        ${showMenu 
+            ? "left-0" 
+            : "-left-full"}`
+            
+        }>{/* Si showMenu esta positivo (?) hace una cosa y sino la otra (:) */}
             <div>
                 <ul className='pl-4'>
                     <li>
